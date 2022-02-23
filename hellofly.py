eb602bf2ff82e3402186f038d71cf01d7b15b9d5
William Gerard <whgerard1@gmail.com>
2022-02-22 22:41:30 -0600

    Add connection to fly postgres database

--- hellofly.py
+++ hellofly.py
@@ -1,9 +1,31 @@
+from curses import echo
 from flask import Flask, render_template
+import sqlalchemy
 from werkzeug.middleware.dispatcher import DispatcherMiddleware
 from prometheus_client import Summary, make_wsgi_app
 import time
+import os
+from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String
 
 app = Flask(__name__)
+
+# after attaching the app to the postgres db app,
+# fly makes the connection url available in this env var
+DB_CONN_STRING = os.environ['DATABASE_URL']
+
+# fly uses "postgres://...", but sqlalchemy wants "postgresql://..."
+sqlalchemy_conn_string = DB_CONN_STRING[:8] + 'ql' + DB_CONN_STRING[8:]
+
+engine = create_engine(sqlalchemy_conn_string, echo=True)
+meta = MetaData()
+
+# Create simple users table to verify the connection
+users = Table(
+   'users', meta, 
+   Column('id', Integer, primary_key = True), 
+   Column('username', String), 
+)
+meta.create_all(engine)
 
 # Add prometheus wsgi middleware to route /metrics requests
 app.wsgi_app = DispatcherMiddleware(app.wsgi_app, {
